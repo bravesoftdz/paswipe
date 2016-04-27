06ed083c95adf3a4e22f665ecdbbcfe9e63e0b05
sa <stievie2006@gmail.com>
2016-04-27 08:23:56 +0200

    Added Win64 configuration

--- 00compress.bat
+++ 00compress.bat
@@ -1,4 +1,4 @@
 @echo off
 
-if exist wipe.zip del wipe.zip
-"%PROGRAMFILES%\7-ZIP\7z.exe" a -tzip -mx wipe.zip @ziplst
+if exist wipe-win32.zip del wipe-win32.zip
+"%PROGRAMFILES%\7-ZIP\7z.exe" a -tzip -mx wipe-win32.zip @ziplst
